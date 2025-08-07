b06c8cfa9d035abe3d3a0df4a2edb606187cdc1a
ricardo.andrade <ricardo.andrade05@gmail.com>
2025-08-07 08:41:55 -0300

    Commit de configuracao

---
+++ src/test/java/com/pessoal/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pessoal.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
