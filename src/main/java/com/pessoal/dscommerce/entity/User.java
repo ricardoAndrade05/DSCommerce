9a6de8246bbdb06512941cd98c947301919a759a
ricardo.andrade <ricardo.andrade05@gmail.com>
2025-08-07 22:41:15 -0300

    Aula: Entidade User, banco H2

---
+++ src/main/java/com/pessoal/dscommerce/entity/User.java
@@ -0,0 +1,31 @@
+package com.pessoal.dscommerce.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+	private String email;
+	private String phone;
+	private LocalDate birthDate;
+	private String password;
+
+}
