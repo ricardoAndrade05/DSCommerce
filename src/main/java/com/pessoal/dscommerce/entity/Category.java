4305c9ae1444105a959ba7c3d1d6749f1412f1c9
ricardo.andrade <ricardo.andrade05@gmail.com>
2025-08-11 11:23:05 -0300

    Aula: Muitos-para-muitos, column unique e text

--- src/main/java/com/pessoal/dscommerce/entity/Category.java
+++ src/main/java/com/pessoal/dscommerce/entity/Category.java
@@ -1,15 +1,13 @@
 package com.pessoal.dscommerce.entity;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,22 +17,17 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "tb_user")
-public class User {
-
+@Table(name = "tb_category")
+public class Category {
+	
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
-	@Column(unique = true)
-	private String email;
 	
 	private String name;
-	private String phone;
-	private LocalDate birthDate;
-	private String password;
 	
-	@OneToMany(mappedBy = "client")
-	private List<Order> orders = new ArrayList<>();
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 
 }
---
+++ src/main/java/com/pessoal/dscommerce/entity/Product.java
@@ -0,0 +1,43 @@
+package com.pessoal.dscommerce.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_product")
+public class Product {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(columnDefinition = "TEXT")
+	private String description;
+	private String name;
+	private Double price;
+	private String imgUrl;
+	
+	@ManyToMany
+	@JoinTable(name = "tb_product_category",
+		joinColumns = @JoinColumn(name = "product_id"),
+		inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
+
+}
